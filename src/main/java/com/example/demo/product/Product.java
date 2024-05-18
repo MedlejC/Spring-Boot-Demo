c669a1c8d458ce7dd3aaf9f27e58020e53db99e9
MedlejC <charbel.medlej@lau.edu>
2024-05-18 18:35:46 +0300

    Implemented getters and setters

--- src/main/java/com/example/demo/product/Product.java
+++ src/main/java/com/example/demo/product/Product.java
@@ -20,7 +20,6 @@ public class Product {
     }
 
     // Constructor #2: Contains all the attributes
-
     public Product(Long id,
                    String name,
                    String description,
@@ -33,6 +32,71 @@ public class Product {
         this.creation_date = creation_date;
     }
 
+    // Constructor #3: With no ID (because the database will generate the ID for us)
+    public Product(String name,
+                   String description,
+                   String category,
+                   LocalDateTime creation_date) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.creation_date = creation_date;
+    }
+
 
     // Getters & Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(LocalDateTime creation_date) {
+        this.creation_date = creation_date;
+    }
+
+
+    // toString method:
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", creation_date=" + creation_date +
+                '}';
+    }
 }
